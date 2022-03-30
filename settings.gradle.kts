ac3c332aba92fa8b2ba7c7bfb9eaf03745c0fce1
mamata.gelanee <mamata.gelanee@imaginecurve.com>
2022-03-30 12:05:55 +0100

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TripMate"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/andigeeky/tripmate/Platform.kt
@@ -0,0 +1,5 @@
+package com.andigeeky.tripmate
+
+expect class Platform() {
+    val platform: String
+}
